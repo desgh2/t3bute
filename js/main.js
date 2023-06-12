511733314f0f8992c10e38861fd2186e02506546
desgh2 <wdesgit@gmail.com>
2023-06-12 22:09:12 +0300

    all styles

--- js/main.js
+++ js/main.js
@@ -4,25 +4,11 @@ $(function () {
         jQuery.fn.clickToggle = function (a, b) {
             return this.on("click", function (ev) { [b, a][this.$_io ^= 1].call(this, ev) })
         };
-
-        // TEST:
+        
         $('#toggle-menu').clickToggle(function (ev) {
             $('#primary-menu').css('display', 'flex');
         }, function (ev) {
             $('#primary-menu').hide();
         });
-
-        /* $('#toggle-menu').toggle(function() {
-            $('#primary-menu').show();
-        }, function() {
-            $('#primary-menu').hide();
-        }); */
-
-       /*  $("#toggle-menu").toggle(function () {
-            $('#primary-menu').show();
-        }, function () {
-            $('#primary-menu').hide();
-        }); */
-
     });
 });
